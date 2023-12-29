236ec29113a3a6f9a9eb63b46a8fb1b4aa1c3267
lax <laxlax@tutanota.com>
2023-12-29 08:34:29 +0800

    change

--- run.sh
+++ run.sh
@@ -1,1 +1,10 @@
-gcc nutation_test.c -IC:/cygwin64/home/75699/include/ -LC:/cygwin64/home/75699/lib/ -lsofa_c -lm -o t_t
+ # @Description: 
+ # @Version: 1.0.0
+ # @Author: lax
+ # @Date: 2023-12-20 00:29:19
+ # @LastEditors: lax
+ # @LastEditTime: 2023-12-29 08:33:52
+### 
+gcc nutation_test.c -IC:/cygwin64/home/75699/include/ -LC:/cygwin64/home/75699/lib/ -lsofa_c -lm -o t_t###
+
+
